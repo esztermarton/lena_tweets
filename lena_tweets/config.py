4aa5b646519afbcd6da9da758f4f577c60e78055
Rebeka Marton <rebeka.marton@eigentech.com>
2020-12-20 20:51:40 +0100

    adjusting path to be absolute

--- lena_tweets/config.py
+++ lena_tweets/config.py
@@ -1,12 +1,12 @@
 TIMESTAMP_FORMAT = "%d-%m-%Y"
-PARTICIPANTS_QUEUE = "data/{}_participants_id_queue.txt"
-DAILY_FRIENDS_CHECK_PATH = "data/{}_users_friends.csv"
-STUDY_START_PATH = "data/{}_users_study_start.csv"
-STUDY_INPUT_START_PART = "data/study_input.txt"
-STUDY_END_PATH = "data/{}_users_study_end.csv"
-TWEET_HISTORY = "data/tweet_history.csv"
-USER_TRACKER_PATH = "data/user_tracker.csv"
-DAILY_TWEETS_PATH = "data/{}_tweets.csv"
+PARTICIPANTS_QUEUE = "/app/data/{}_participants_id_queue.txt"
+DAILY_FRIENDS_CHECK_PATH = "/app/data/{}_users_friends.csv"
+STUDY_START_PATH = "/app/data/{}_users_study_start.csv"
+STUDY_INPUT_START_PART = "/app/data/study_input.txt"
+STUDY_END_PATH = "/app/data/{}_users_study_end.csv"
+TWEET_HISTORY = "/app/data/tweet_history.csv"
+USER_TRACKER_PATH = "/app/data/user_tracker.csv"
+DAILY_TWEETS_PATH = "/app/data/{}_tweets.csv"
 
 # Fill out before deploying!
 CREDS = []
